11243c6458d4a71c52a7d131ea2bfea23ca8d62b
Karla Pocasangre <karlahernandezpocasangre@gmail.com>
2023-09-15 17:15:26 -0600

    Calculo de Nota de Presentacion y Nota Final terminado

--- CalculadoraUSO.cpp
+++ CalculadoraUSO.cpp
@@ -5,11 +5,33 @@ int main()
 {
     // Declaracion de Variables
     int calcular, porcentajeLab, porcentajePar;
-    float notaL1, notaL2, notaP1, notaP2, Npresentacion, PLab, PPar, NFLabs, NFPars, notaL3;
-    
+    float notaL1, notaL2, notaP1, notaP2, PLab, PPar, NFLabs, NFPars, notaL3, ExamenFinal;
+    double Npresentacion, NFexamenfinal, NotaFinal;
+
+    // Ingreso de Datos
+    cout << "Ingrese el porcentaje (sin el %) asignado de los laboratorios: ";
+    cin >> porcentajeLab;
+    cout << "Ingrese el porcentaje (sin el %) asignado de los Parciales: ";
+    cin >> porcentajePar;
+    cout << "Ingrese la nota de los laboratorios:" << endl;
+    cin >> notaL1;
+    cin >> notaL2;
+    cin >> notaL3;
+    cout << "Ingrese la nota de los Parciales:" << endl;
+    cin >> notaP1;
+    cin >> notaP2;
+
+    // Caluculo Porcentajes
+    PLab = (porcentajeLab / 100.0);
+    PPar = (porcentajePar / 100.0);
+
+    // Caluculo de Nota de Presentacion
+    NFLabs = ((notaL1 + notaL2 + notaL3) * PLab);
+    NFPars = ((notaP1 + notaP2) * PPar);
+    Npresentacion = NFLabs + NFPars;
 
     // Decision del Usuario
-    cout << "Elija si Calcular la nota de Presentacion (1) o La nota Final y Nota de Presentacion (2): ";
+    cout << "Elija si Calcular la nota de Presentacion (1) o La nota Final (2): ";
     cin >> calcular;
 
     // Switch
@@ -17,33 +39,23 @@ int main()
     {
     case (1):
 
-        // Ingreso de Datos
-        cout << "Ingrese el porcentaje asignado de los laboratorios: (sin el %)";
-        cin >> porcentajeLab;
-        cout << "Ingrese el porcentaje asignado de los Parciales: (sin el %)";
-        cin >> porcentajePar;
-        cout << "Ingrese la nota de los laboratorios:" << endl;
-        cin >> notaL1;
-        cin >> notaL2;
-        cin >> notaL3;
-        cout << "Ingrese la nota de los Parciales:" << endl;
-        cin >> notaP1;
-        cin >> notaP2;
-
-        //Caluculo Porcentajes
-        PLab=(porcentajeLab/100.0);
-        PPar=(porcentajePar/100.0);
-
-        NFLabs=((notaL1+notaL2+notaL3)*PLab);
-        NFPars=((notaP1+notaP2)*PPar);
-        Npresentacion= NFLabs+NFPars;
-        //Salida de Datos
-        //cout << "El porcentaje de los Labs es: "<< PLab << endl;
-        cout << "nota presentacion "<< Npresentacion << endl;
+        // Salida de Datos
+        cout << " la nota de presentacion es" << Npresentacion << endl;
 
         break;
+
     case (2):
 
+        // Ingreso de Datos Examen Final
+        cout << "Ingrese la nota del Examen Final" << endl;
+        cin >> ExamenFinal;
+
+        //Calculando la Nota final
+        NFexamenfinal = (ExamenFinal * 0.4);
+        NotaFinal = (Npresentacion + NFexamenfinal);
+        cout << "La nota Final es: " << NotaFinal << endl;
+
+    
         break;
     default:
         cout << "Ingrese un numero valido" << endl;
