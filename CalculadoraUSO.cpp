dfe53c438e848c7207ea0615eded0497da71a775
Karla Pocasangre <karlahernandezpocasangre@gmail.com>
2023-09-14 23:36:01 -0600

    Agregando Archivo cpp

---
+++ CalculadoraUSO.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int promedio;
+    cout<<"Ingrese su promedio"<<endl;
+    cin>>promedio;
+cout<<"Su promedio es: "<<promedio<<endl;
+    return 0;
+}
